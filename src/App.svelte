6bc4e7c578143567d3ad440c1c4977fb40b2f55c
Esther Lurie <elurie3@bloomberg.net>
2024-03-01 14:24:14 -0500

    patch to 0.0.1

--- src/App.svelte
+++ src/App.svelte
@@ -5,9 +5,10 @@
 
 <main>
   <div class="header">
-    <h1 class="name">esti</h1>
-    <img class="logo" alt="I <3 you!" src={heart} />
+    <h1 class="name">esti lurie</h1>
+    <h2 class="name">/ɛsti lɜri/</h2>
   </div>
+  <br />
   <div class="links">
     <NewTabLink url={"https://www.github.com/estherlurie"} text={"developer"} />
     <NewTabLink
@@ -24,20 +25,16 @@
 
 <style>
   .header {
-    display: flex;
+    display: block;
   }
 
   .name {
     color: #db3030;
+    margin: 0;
   }
 
   .links {
     font-size: 1.25em;
-  }
-
-  .logo {
-    width: 50px;
-    margin-left: 10px;
   }
 
   @media (prefers-color-theme: light) {
